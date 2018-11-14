a72abcba0592287aecbfe95e3865b72894bc17f6
Alex Siqueira Lacerda <Lequizi@gmail.com>
2018-11-14 16:00:44 -0200

    changing weights

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,23 +1,23 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-const unsigned short MP_INICIAL_GUERREIRO = 10;
-const unsigned short MP_INICIAL_ARQUEIRO = 20;
+const unsigned short MP_INICIAL_GUERREIRO = 30;
+const unsigned short MP_INICIAL_ARQUEIRO = 30;
 const unsigned short MP_INICIAL_CAVALEIRO = 30;
 
 const unsigned short HP_INICIAL_PILAR_ESPADA = 100;
-const unsigned short HP_INICIAL_PILAR_ARCO = 80;
-const unsigned short HP_INICIAL_PILAR_LANCA = 70;
+const unsigned short HP_INICIAL_PILAR_ARCO = 100;
+const unsigned short HP_INICIAL_PILAR_LANCA = 100;
 
 const unsigned short QUANTIDADE_OSSOS_POR_ITEM = 10;
 const unsigned short QUANTIDADE_METAL_POR_ITEM = 20;
 
-const unsigned short OSSOS_CRIAR_GUERREIRO = 20;
-const unsigned short OSSOS_CRIAR_ARQUEIRO = 40;
-const unsigned short OSSOS_CRIAR_CAVALEIRO = 60;
+const unsigned short OSSOS_CRIAR_GUERREIRO = 30;
+const unsigned short OSSOS_CRIAR_ARQUEIRO = 30;
+const unsigned short OSSOS_CRIAR_CAVALEIRO = 30;
 
 const unsigned short METAL_CRIAR_PILAR_ESPADA = 60;
-const unsigned short METAL_CRIAR_PILAR_ARCO = 80;
-const unsigned short METAL_CRIAR_PILAR_LANCA = 100;
+const unsigned short METAL_CRIAR_PILAR_ARCO = 60;
+const unsigned short METAL_CRIAR_PILAR_LANCA = 60;
 
 #endif
