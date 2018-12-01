fda177f029171b94184c5ae19032181c467dd695
alexander <aaalexander95@gmail.com>
2018-11-30 23:56:41 -0200

    Cpplint da main

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,20 @@
+//// "Copyright 2018 Jan_of_Empires"
 #include "../include/Game.hpp"
 #include "../include/Graphics.hpp"
 
-int main(int argc, char *args[])
-{
+int main(int argc, char *args[]) {
     Graphics *graphics = new Graphics();
     Game *game = new Game();
 
-	if( !graphics->init() ){
-		printf( "Falha ao inicializar!\n" );
-	}
-	else{
-		//Carrega mídia
-		if( !graphics->loadMedia() ){
-			printf( "Nao foi possivel carregar a midia!\n" );
-		}
-		else{
+    if (!graphics->init()) {
+        printf("Falha ao inicializar!\n");
+    } else {
+        // Carrega mídia
+        if (!graphics->loadMedia()) {
+            printf("Nao foi possivel carregar a midia!\n");
+        } else {
             game->playGame(GAME_MENU);
-    	}
+        }
         graphics->close();;
     }
 
